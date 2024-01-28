37d0d8515c6dcfc5452addde5b30908078fd1df4
elxp101t3r <eliotx10101@outlook.com>
2024-01-28 23:13:48 +0200

    Fix the style a little

--- UI.py
+++ UI.py
@@ -16,23 +16,25 @@ class Win(Tk):
         self.geometry('670x550')
         self.cnv = Canvas(width=600, height=400, bg='white')
         self.cnv.grid(column=0, row=0, columnspan=2, padx=35)
-        self.cnv.create_rectangle((100, 100), (500, 500), fill='#d6b37e')
+        self.cnv.create_rectangle((100, 100), (500, 500), fill='green')
         self.card_title = self.cnv.create_text(
             (300, 200),
             text="Front:",
             font=(('Arial', 18, 'italic')),
-            fill='black'
+            fill='white'
         )
         self.card_word = self.cnv.create_text(
             (300, 250),
             text="Q1",
             font=("Arial", 20, "bold"),
-            fill='black'
+            fill='white'
         )
-        self.known_btn = Button(text='Known', bootstyle='warning-outline', command=self.next_card)
+        self.known_btn = Button(text='Known', bootstyle='success-outline', command=self.next_card)
         self.known_btn.grid(column=1, row=2, pady=20)
-        self.unknown_btn = Button(text='Unknown', bootstyle='primary-outline', command=self.next_card)
+        self.unknown_btn = Button(text='Unknown', bootstyle='danger-outline', command=self.next_card)
         self.unknown_btn.grid(column=0, row=2, pady=20)
+        
+        self.next_card()
         self.mainloop()
         
         
